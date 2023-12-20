fa7054cb67844788e9ccd484508207ec1cdced6d
vladimirnovacek <novacek.vladimir@gmail.com>
2023-12-20 09:25:35 +0100

    Closing this window will end the app. Comments added

--- lobby.py
+++ lobby.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import typing
 
+from twisted.internet import reactor
+
 import config
 from game_data import GameData
 from interfaces import Observer
@@ -24,7 +26,7 @@ class Lobby(tk.Toplevel, Observer):
         """
 
         def __init__(self, lobby):
-            self.master = lobby
+            self.master: Lobby = lobby
             self._game_data = {
                 "players": [
                     {
@@ -38,11 +40,19 @@ class Lobby(tk.Toplevel, Observer):
                     "my_id": tk.IntVar(lobby, -1, name=f"misc.my_id"),
                 }
             }
+            """ Structure of the data is similar to the structure in the GameData class """
 
         def __getitem__(self, item):
             return self._game_data[item]
 
-        def __contains__(self, item):
+        def __contains__(self, item: typing.Iterable) -> bool:
+            """
+            Expects a tuple (or other iterable object) with keys in the order (section, item, [attribute])
+            :param item: tuple of keys
+            :type item: Iterable
+            :return: The given key exists
+            :rtype: bool
+            """
             if isinstance(item, typing.Iterable):
                 try:
                     value = self
@@ -51,24 +61,34 @@ class Lobby(tk.Toplevel, Observer):
                 except (IndexError, KeyError):
                     return False
                 return True
-            try:
-                if isinstance(item, typing.Iterable):
-                    _ = self
-                    for i in item:
-                        _ = self[i]
-                else:
-                    self.__getitem__(item)
-            except (ValueError, KeyError):
-                return False
-            return True
+            return False
 
         def select(self, keys: typing.Iterable) -> tk.IntVar | tk.StringVar | tk.BooleanVar:
+            """
+            Returns a tkinter variable by keys. Keys are in an iterable in the order of (section, item, [attribute]).
+            If the key is not present, raises an exception.
+            :param keys: keys in the order of (section, item, [attribute])
+            :type keys: Iterable
+            :return: Tkinter variable
+            :rtype: tk.IntVar | tk.StringVar | tk.BooleanVar
+            """
             item = self
             for key in keys:
                 item = item[key]
             return item
 
-        def update(self, *, section, item, attribute=None, value):
+        def update(self, *, section: str, item: str | int, attribute: str | None = None, value: typing.Any) -> None:
+            """
+            Updates a value of a variable specified by the method attributes.
+            :param section: Section is one of ("fields", "players", "misc")
+            :type section: str
+            :param item:
+            :type item: str | int
+            :param attribute: With section == "misc" the attribute = None
+            :type attribute: str | None
+            :param value: Value that will be stored into the variable
+            :type value: Any
+            """
             if attribute:
                 keys = (section, item, attribute)
             else:
@@ -86,9 +106,12 @@ class Lobby(tk.Toplevel, Observer):
     def __init__(self, master: tk.Tk, message_factory: MessageFactory, game_data: GameData):
         super().__init__(master=master)
         self.data: Lobby.Data = self.Data(self)
-        self.game_data = game_data
+        """ Class contains tkinter variables for this window. """
+        self.game_data: GameData = game_data
+        """ Link to game data """
         self.game_data.register(self)
         self.message_factory = message_factory
+        """ Link to a message factory """
         self.tokens_list: list = config.available_tokens
         """ List of available tokens """
         self.table: ttk.Frame = ttk.Frame(self)
@@ -96,15 +119,31 @@ class Lobby(tk.Toplevel, Observer):
         self.table_elements: list[typing.Any] = self._fill_table_elements()
         """ List of table elements, such as name fields, token comboboxes, etc. """
         self.buttons_frame: ttk.Frame = ttk.Frame(self)
-        """  Frame containing buttons """  # TODO buttons not implemented
+        """  Frame containing buttons """
         self.buttons: list[typing.Any] = self._get_buttons()
         self.table.pack()
         self.buttons_frame.pack()
+        self.protocol("WM_DELETE_WINDOW", self._destroy)
 
-    def update_value(self, section, item, attribute, value):
+    def update_value(self, *, section: str, item: str | int, attribute: str | None = None, value: typing.Any) -> None:
+        """
+        Updates a value of a variable specified by the method attributes.
+        :param section: Section is one of ("fields", "players", "misc")
+        :type section: str
+        :param item:
+        :type item: str | int
+        :param attribute: With section == "misc" the attribute = None
+        :type attribute: str | None
+        :param value: Value that will be stored into the variable
+        :type value: Any
+        """
         self.data.update(section=section, item=item, attribute=attribute, value=value)
 
-    def set_my_player_id(self):
+    def set_my_player_id(self) -> None:
+        """
+        This method is called automatically when the player_id value is available. It causes that only the widgets
+        of the player are changeable and adds tracing for the changes.
+        """
         my_id = self.data["misc"]["my_id"].get()
         if my_id == -1:
             print("My_id is not set yet.")
@@ -123,16 +162,19 @@ class Lobby(tk.Toplevel, Observer):
         ready_checkbox.var = self.data.select(key + ["ready"])
         ready_checkbox.var.trace_add("write", self._notify)
 
-    def _notify(self, name: str, something: str, mode: str):
-        keys: list[str | int] = name.split(".")
-        keys[1] = int(keys[1])
-        value = self.data.select(keys).get()
-        self.message_factory.send(
-            "user_info",
-            {"section": keys[0], "item": keys[1], "attribute": keys[2], "value": value}
-        )
+    def _destroy(self) -> None:
+        """
+        Ends the entire app when this window is closed.
+        """
+        self.master.destroy()
+        reactor.stop()
 
-    def _fill_table_elements(self) -> list[list]:
+    def _fill_table_elements(self) -> list[list[tk.Widget]]:
+        """
+        Returns a table with tkinter elements. These elements master is self.table frame.
+        :return: list[list]
+        :rtype: Widget
+        """
         table_elements: list[list] = [[
             tk.Label(self.table, text='Name'),
             tk.Label(self.table, text='Token'),
@@ -164,14 +206,34 @@ class Lobby(tk.Toplevel, Observer):
                 table_elements[i + 1][j].grid(column=j, row=i + 1, padx=5, pady=5)  # Place element to the grid
         return table_elements
 
-    def _get_buttons(self):
+    def _get_buttons(self) -> list[tk.Widget]:
+        """
+        Returns a list with buttons. The master for the buttons is self.buttons_frame
+        :return: list of buttons
+        :rtype: list[Widget]
+        """
         self.buttons_frame.configure(borderwidth=5)
-        start_btn = ttk.Button(self.buttons_frame, text="Start Game", command=self._start_game)
+        start_btn = ttk.Button(
+            self.buttons_frame, text="Start Game", command=lambda: self.message_factory.send("start_game")
+        )
         buttons = [start_btn]
         start_btn.pack()
         return buttons
 
-    def _start_game(self):
-        self.master.deiconify()
-        self.destroy()
-
+    def _notify(self, name: str, something: str, mode: str) -> None:
+        """
+        Sends a message to the server when the value of a tkinter variable specified by the name attribute changes.
+        :param name: The name of a tkinter variable
+        :type name: str
+        :param something: Mandatory attribute. It's not used by this method
+        :type something: str
+        :param mode: Mandatory attribute. It's not used by this method
+        :type mode: str
+        """
+        keys: list[str | int] = name.split(".")
+        keys[1] = int(keys[1])
+        value = self.data.select(keys).get()
+        self.message_factory.send(
+            "user_info",
+            {"section": keys[0], "item": keys[1], "attribute": keys[2], "value": value}
+        )
