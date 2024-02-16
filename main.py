dbb5fcd2d389208c069c051406c8267fe96df1de
vladimirnovacek <novacek.vladimir@gmail.com>
2024-02-16 12:02:49 +0100

    Updated to match Messenger's signature

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 from twisted.internet import tksupport, reactor
 
 import config
-from data_parser import MessageParser
 from client import ClFactory
 from game_data import GameData
 from game_window import GameWindow
-from message_factory import MessageFactory
+from messenger import Messenger
 
 
 def start():
@@ -15,13 +14,13 @@ def start():
     :return:
     """
     game_data = GameData()
-    message_factory = MessageFactory(game_data)
+    message_factory = Messenger()
     game_window = GameWindow(message_factory, game_data)
     # noinspection PyUnresolvedReferences
     game_window.protocol("WM_DELETE_WINDOW", reactor.stop)
     tksupport.install(game_window)
     # noinspection PyUnresolvedReferences
-    reactor.connectTCP(config.server_address, config.server_port, ClFactory(MessageParser(game_data), message_factory))
+    reactor.connectTCP(config.server_address, config.server_port, ClFactory(message_factory))
     # noinspection PyUnresolvedReferences
     reactor.run()
 
