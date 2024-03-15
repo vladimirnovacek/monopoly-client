9584fd90c6a615a185479a30e77a46627f8cb53a
vladimirnovacek <novacek.vladimir@gmail.com>
2024-03-15 14:58:31 +0100

    Preparations for showing property dialog when property clicked

--- gameboard.py
+++ gameboard.py
@@ -36,7 +36,10 @@ class GameBoard(tk.Canvas):
         self.dice: Dice = Dice(self)
         self.dice.draw()
 
+        self.token_field_positions = config.token_field_positions
         self.field_coordinates = config.field_coordinates
+
+        self.bind("<Button-1>", self._canvas_clicked)
 
         # self.card: dict[str, Card | PropertyCard] = {
         #     "chance_cc": ChanceCcCard(self),
@@ -44,6 +47,16 @@ class GameBoard(tk.Canvas):
         #     "railroad": RailroadCard(self),
         #     "utility": UtilityCard(self)
         # }
+
+    def _canvas_clicked(self, event: tk.Event) -> None:
+        x, y = event.x, event.y
+        for i, field in enumerate(self.field_coordinates):
+            x1, y1 = field[0]
+            x2, y2 = field[1]
+            if x1 <= x <= x2 and y1 <= y <= y2:
+                print(f"Clicked field number {i}")
+                return
+        print("Clicked canvas")
 
     def moveto(self, tagOrId, x = 0, y = 0,
                anchor: typing.Literal["nw", "n", "ne", "w", "center", "e", "sw", "s", "se"] = tk.NW):
@@ -61,7 +74,7 @@ class GameBoard(tk.Canvas):
     def start_game(self):
         for player_id, player in self.master.game_data.players.items():
             token = self.root.tokens[config.tokens.index(player["token"])]
-            x, y = self.field_coordinates[player["field"]]
+            x, y = self.token_field_positions[player["field"]]
             token_id = self.create_image(x, y, anchor=tk.CENTER, image=token)
             self.tokens[player_id] = token_id
 
@@ -71,20 +84,17 @@ class GameBoard(tk.Canvas):
         else:
             self.tag_unbind("dice", "<Button-1>")
 
-    def get_field_coordinate(self, field: int) -> tuple[int, int]:
-        return self.field_coordinates[field]
-
     def _roll(self, *args):
         self.root.messenger.send("roll")
 
     def move_token(self, player_id: int, end_field_id: int, *, backwards: bool = False, directly: bool = False):
         def step(to_id: int):
             if backwards:
-                from_id = 0 if to_id == len(self.field_coordinates) - 2 else to_id + 1
+                from_id = 0 if to_id == len(self.token_field_positions) - 2 else to_id + 1
             else:
-                from_id = len(self.field_coordinates) - 2 if to_id == 0 else to_id - 1
-            x0, y0 = self.field_coordinates[from_id]
-            x1, y1 = self.field_coordinates[to_id]
+                from_id = len(self.token_field_positions) - 2 if to_id == 0 else to_id - 1
+            x0, y0 = self.token_field_positions[from_id]
+            x1, y1 = self.token_field_positions[to_id]
             dx, dy = x1 - x0, y1 - y0
             self.move(self.tokens[player_id], dx, dy)
 
@@ -92,7 +102,7 @@ class GameBoard(tk.Canvas):
         field_id = self.root.game_data.players[player_id]["field"]
 
         if directly:
-            x, y = self.field_coordinates[end_field_id]
+            x, y = self.token_field_positions[end_field_id]
             self.moveto(self.tokens[player_id], x, y, anchor=tk.CENTER)
             return
 
@@ -108,8 +118,8 @@ class GameBoard(tk.Canvas):
         else:
             while field_id != end_field_id:
                 field_id += 1
-                if field_id >= len(self.field_coordinates) - 1:
-                    field_id %= len(self.field_coordinates) - 1
+                if field_id >= len(self.token_field_positions) - 1:
+                    field_id %= len(self.token_field_positions) - 1
                 step(field_id)
                 self.update()
                 sleep(delay)
